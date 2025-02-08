8e584b78e3b91ef0b3b906a4b0c0e60ac6acedc2
sonu kumar <kumarshanu062@gmail.com>
2025-02-08 21:27:31 +0530

    implemented the file opening

---
+++ app/file/filehandler.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("data.txt")
+	if err != nil {
+		fmt.Println("could not open File", err)
+		return
+	}
+	defer file.Close()
+
+	sc := bufio.NewScanner(file)
+	for sc.Scan() {
+		fmt.Println(sc.Text())
+	}
+
+}
